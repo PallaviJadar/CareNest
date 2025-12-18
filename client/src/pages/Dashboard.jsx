e4429b532b4d269b03a23bca555735f4afe940c0
PallaviJadar <pallavijadar07@gmail.com>
2025-12-19 01:46:43 +0530

    Fix: Import Link in Dashboard and replace all remaining localhost URLs

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from 'react-router-dom';
 import API_BASE_URL from '../utils/api';
 
 import ChatBox from '../components/ChatBox';
